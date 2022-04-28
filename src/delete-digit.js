7204f0817a592a9fdd880cd663a2be0d601da7bc
Alexei Ivashkevych <ivashkevich1991@gmail.com>
2022-04-28 19:29:15 +0300

    complete encode-line

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
+
+function deleteDigit(n) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // let nStr = n + '';
+  // let a = [];
+  // for (let i=0 ; i< nStr.length ;i++){
+  //   a.push(+nStr[i]);
+  // }
+  // console.log(a)
+  // let min = Math.min(...a);
+  // console.log(min);
+  // for(let j=0;j<a.length;j++){
+  //   if(a[j] == min){
+  //     a.splice(j,1)
+  //   }
+  // }
+  // // a.sort((a, b) => b - a);
+  // let b = +a.join('')
+  // return b;
 }
+// deleteDigit(125);
 
 module.exports = {
   deleteDigit
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine( str ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+  let arr = [],
+      counter = 1;
 
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            counter++;
+        } else {
+            arr.push([counter, str[i]]);
+            counter = 1;
+        }
+    }
+    for(let i = 0;i<arr.length;i++){
+      if(arr[i][0] == 1){
+        arr[i].shift();
+      }
+    }
+    let b = arr.flat(1).join('')
+   return b;
+}
+encodeLine('aabbbc')
 module.exports = {
   encodeLine
 };
